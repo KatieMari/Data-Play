3437a160cc073707ad3124a146f2ea6cfcf08c8e
KatieMari <katie.glover@student.norwichuni.ac.uk>
2024-10-29 11:34:53 +0000

    added breed list

--- Java-Script/database.js
+++ Java-Script/database.js
@@ -1,5 +1,9 @@
+const breedSelect = document.getElementById("breed-select");
+const subBreedSelect = document.getElementById("sub-breed-select");
+
+
 async function fetchBreedPossibilties() {
-    const breedListUrl = "https://dog.ceo/api/breeds/list/all";
+    const breedListUrl = "https://dog.ceo/api/breeds/list";
 
     // try to fetch url and Display Error Otherwise
     try {
@@ -14,10 +18,50 @@ async function fetchBreedPossibilties() {
         // Transform Response to JSON
         const json = await response.json();
 
-        console.log(json);
+        const breedsList = json.message;
 
-    } catch (error) {
+        for(const breed of breedsList) {
+            // Populate Breed Select - Creates New Option Element
+            const newOption = document.createElement("option");
+            newOption.text = breed;
+            breedSelect.options.add(newOption, breed);
+        }
+
+    } catch(error) {
         console.error(error);
     }
 
 }
+
+async function fetchRandomDog(){
+    try{
+        // Random Dog URL
+        const randomDogUrl = "https://dog.ceo/api/breeds/image/random";
+
+        // Get Response from Server
+        const response = await fetch(randomDogUrl);
+
+        // Check Response is OK
+        if(!response.ok) {
+            throw new Error("Response status: " + response.status);
+        }
+
+        // Get JSON and Message Objects
+        const json = await response.json();
+        const message = json.message;
+
+        // Update Image with Received Source
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+    
+
+
+
+
+
+// Functions to be Executed at the Beginning of the Code
+fetchBreedPossibilties();
