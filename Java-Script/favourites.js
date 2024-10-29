7af5dbeb3561ff2ba082b38ccc3c00fb0d7910f9
KatieMari <katie.glover@student.norwichuni.ac.uk>
2024-10-29 15:24:04 +0000

    moved h2 out of fave div

--- Java-Script/favourites.js
+++ Java-Script/favourites.js
@@ -7,10 +7,20 @@ const favouritesDiv = document.getElementById("favourites-div")
 // currentImage Stores Image Currently Displayed - Updated in databas.js onFetchRandomDog Function
 let currentImage = "";
 
+// List of all Favourites used to Manage Repetition
+let favouritesList = [];
+
 /**
  * Creates newImage Element and Append it to the Favourites Div
  */
 function onAddFavouriteButtonClick() {
+
+    // Checks Currently Displayed Image is not in the List Already
+    if (favouritesList.includes(currentImage)) return;
+
+    // Adds Current Image to Favourite List
+    favouritesList.push(currentImage);
+
     // Creates newImage and Upate Source
     const newImage = document.createElement("img");
     newImage.src = currentImage;
