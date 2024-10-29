88259b9bbb825e4d008cf48cfe81319e0c5a72c6
KatieMari <katie.glover@student.norwichuni.ac.uk>
2024-10-29 15:57:53 +0000

    added downlaaod

--- Java-Script/favourites.js
+++ Java-Script/favourites.js
@@ -10,6 +10,26 @@ let currentImage = "";
 // List of all Favourites used to Manage Repetition
 let favouritesList = [];
 
+async function downloadImage(imageSrc) {
+    // Image Fetch to get the Specific Image Information
+    const image = await fetch(imageSrc);
+    const imagBlob = await image.blob();
+    const imageURL = URL.createObjectURL(imageBlob);
+
+    const link = document.createElement("a");
+    link.href = imageURL;
+    link.download = "cuteDog.png";
+    link.click();
+}
+
+function onDownloadClick(event) {
+    const clickedButton = event.target;
+    const parentDiv = clickedButton.closest("div");
+    const imageElement = parentDiv.querySelector("img");
+
+
+}
+
 /**
  * Creates newImage Element and Append it to the Favourites Div
  */
@@ -21,14 +41,32 @@ function onAddFavouriteButtonClick() {
     // Adds Current Image to Favourite List
     favouritesList.push(currentImage);
 
+    // Creates Div that will Contain Eveything Favourite Related
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("container", "flex-column", "favourite-card");
+
     // Creates newImage and Upate Source
     const newImage = document.createElement("img");
     newImage.src = currentImage;
     newImage.classList.add("favourite-img");
 
-    // Append as Child
-    favouritesDiv.append(newImage);
+    // Creat download Button
+    const downloadButton = document.createElement("button");
+    downloadButton.innerHTML = "Download";
+
+    // Append newDiv Childs
+    newDiv.append(newImage);
+    newDiv.append(downloadButton);
+
+    // Appends Div to Favourites Div
+    favouritesDiv.append(newDiv);
 }
+
+
+
+
+
+
 
 // Link Events
 addFavouriteButton.onclick = onAddFavouriteButtonClick;
